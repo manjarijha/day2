dd4261f2b25cfcba73f0c168b0fb2f1b5e3698f2
manjari jha <manjari.nitdgp@gmail.com>
2021-07-16 20:00:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// let start= document.getElementById('start').addEventListener('click', function()
+// {
+//     // alert('button clicked');
+//     document.getElementById('main').style.animation = ' mj 5s linear infinite' ;
+//     document.getElementById('main').style.backgroundColor = 'azure';
+// })
+  
+let stop = document.getElementById('stop').addEventListener('click', function()
+{
+    document.getElementById('main').style.animation = 'mj 0s ';
+    document.getElementById('main').style.backgroundColor = 'blueviolet';
+})
+
+
+ let color= document.getElementById('start').addEventListener('click', function()
+ {
+ document.getElementById('main').setAttribute("class","my-animationclass")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
